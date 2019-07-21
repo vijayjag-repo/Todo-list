51523e8c82fbd9f889241c9c3ad5891a651ffc51
Vijay Jagannathan <vijayjag@Vijays-MacBook-Pro.local>
2019-07-21 17:27:02 -0500

    Updated using mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
-var items = [];
+mongoose.connect("mongodb://localhost:27017/todolistdb",{useNewUrlParser: true});
+
+//create items schema
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item({
+  name: "Hello"
+});
+
+const item2 = new Item({
+  name: "there"
+});
+
+var collectionitems = [item1,item2];
+
 app.get("/",function(req,res){
   var today = new Date();
   var options = {
@@ -14,12 +33,32 @@ app.get("/",function(req,res){
     month: "long"
   };
   var day = today.toLocaleDateString("en-US",options);
-  res.render("list",{kindOfDay: day,newlistitem: items});
+  Item.find({},function(err,foundItems){
+    if(foundItems.length===0){
+      Item.insertMany(collectionitems,function(err){
+        if(err){
+          console.log("Error");
+        }
+        else{
+          console.log("Success");
+        }
+      });
+      res.redirect("/");
+    }
+    else{
+      res.render("list",{listTitle: day,newlistitem: foundItems});
+    }
+  });
+
+
 });
 
 app.post("/",function(req,res){
-  var item = req.body.newitem;
-  items.push(item);
+  const itemName = req.body.newitem;
+  const item = new Item({
+    name: itemName
+  });
+  item.save();
   res.redirect("/");
 });
 app.listen(3000,function(){
